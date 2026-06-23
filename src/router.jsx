640a96b6c122e9d311aa744e0ddbd39e7f91ded6
fasalurahmanktt <fasalurahman655@gmail.com>
2026-06-23 14:46:51 +0530

    initial commit

---
+++ src/router.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router";
+
+import Home from "../pages/home/Home";
+import counter from "../pages/counter/counter";
+import About from "../pages/about/About";
+import App from "../App";
+
+export const router = createBrowserRouter([
+  
+  
+  
+    
+      {
+        path: "home",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "counter",
+        element: <counter />,
+      },
+    ],
+
